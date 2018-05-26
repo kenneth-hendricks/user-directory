c13447209fbf3f701e718c093e5a159da23ad7ae
kenneth-hendricks <ken.c.hendricks@gmail.com>
2018-05-26 16:57:00 +1000

    Add UserData to seed the site with data

---
+++ src/UserData.js
@@ -0,0 +1,376 @@
+// Generated from randomuser.me
+
+const UserData =[
+  {
+    "gender": "female",
+    "name": {
+      "title": "mrs",
+      "first": "sara",
+      "last": "colin"
+    },
+    "location": {
+      "street": "6260 rue de gerland",
+      "city": "créteil",
+      "state": "hautes-pyrénées",
+      "postcode": 14087
+    },
+    "email": "sara.colin@example.com",
+    "login": {
+      "username": "beautifulostrich983",
+      "password": "fugazi",
+      "salt": "uHNzyNV7",
+      "md5": "ab13d59cf7f774e1d1a640b215fc6308",
+      "sha1": "82b663afc4000051e924bd2c4b7b765ec32c2c12",
+      "sha256": "7796f26389bf31261f8f9f2ecd59daf4ae83d99adc85d7bd249f19598fc031c1"
+    },
+    "dob": "1983-10-28 04:43:50",
+    "registered": "2011-12-25 05:27:08",
+    "phone": "01-40-61-66-35",
+    "cell": "06-10-64-76-62",
+    "id": {
+      "name": "INSEE",
+      "value": "283974209061 95"
+    },
+    "picture": {
+      "large": "https://randomuser.me/api/portraits/women/14.jpg",
+      "medium": "https://randomuser.me/api/portraits/med/women/14.jpg",
+      "thumbnail": "https://randomuser.me/api/portraits/thumb/women/14.jpg"
+    },
+    "nat": "FR"
+  },
+  {
+    "gender": "female",
+    "name": {
+      "title": "ms",
+      "first": "honória",
+      "last": "da mata"
+    },
+    "location": {
+      "street": "6239 avenida da democracia",
+      "city": "gravataí",
+      "state": "mato grosso",
+      "postcode": 31847
+    },
+    "email": "honória.damata@example.com",
+    "login": {
+      "username": "bluegoose343",
+      "password": "sonja",
+      "salt": "F6XgsesM",
+      "md5": "ed03d3988ff83f1e134478eb826b3787",
+      "sha1": "05857b3dfe56f336a1e1e1cf921d603211f288d6",
+      "sha256": "ffe05cb331b98ad1deb048cd5b3f5ccedcb541805f1d40dcf4f4f100daaa210c"
+    },
+    "dob": "1955-07-11 09:36:06",
+    "registered": "2006-10-18 06:33:42",
+    "phone": "(20) 3209-7391",
+    "cell": "(41) 2395-6739",
+    "id": {
+      "name": "",
+      "value": null
+    },
+    "picture": {
+      "large": "https://randomuser.me/api/portraits/women/74.jpg",
+      "medium": "https://randomuser.me/api/portraits/med/women/74.jpg",
+      "thumbnail": "https://randomuser.me/api/portraits/thumb/women/74.jpg"
+    },
+    "nat": "BR"
+  },
+  {
+    "gender": "female",
+    "name": {
+      "title": "miss",
+      "first": "indie",
+      "last": "moore"
+    },
+    "location": {
+      "street": "8823 domain road",
+      "city": "lower hutt",
+      "state": "gisborne",
+      "postcode": 94257
+    },
+    "email": "indie.moore@example.com",
+    "login": {
+      "username": "crazyduck673",
+      "password": "rubber",
+      "salt": "52o4CU4E",
+      "md5": "86cdfd3ffdeb9b74775358f51369d865",
+      "sha1": "5152b70ae7081a3e7286cacb9b5bccc3728db934",
+      "sha256": "6cf21124e18b210cfd41d9a4ff194395a8d3d5038749277abe192346d0b5f7c8"
+    },
+    "dob": "1990-02-06 02:53:29",
+    "registered": "2005-10-10 06:07:00",
+    "phone": "(776)-520-7999",
+    "cell": "(669)-203-8779",
+    "id": {
+      "name": "",
+      "value": null
+    },
+    "picture": {
+      "large": "https://randomuser.me/api/portraits/women/86.jpg",
+      "medium": "https://randomuser.me/api/portraits/med/women/86.jpg",
+      "thumbnail": "https://randomuser.me/api/portraits/thumb/women/86.jpg"
+    },
+    "nat": "NZ"
+  },
+  {
+    "gender": "female",
+    "name": {
+      "title": "ms",
+      "first": "britney",
+      "last": "payne"
+    },
+    "location": {
+      "street": "5020 church street",
+      "city": "gloucester",
+      "state": "rutland",
+      "postcode": "I5 8JA"
+    },
+    "email": "britney.payne@example.com",
+    "login": {
+      "username": "crazybear824",
+      "password": "chapman",
+      "salt": "qBfT1bKK",
+      "md5": "e2bba59654bde2c75bdce9147244e228",
+      "sha1": "64358c9819d9b223024f597e2cbe8f1717ca3df7",
+      "sha256": "2ff8c154234b8e1b5d135ed577695948dd79e022a6e229621ed4b23b4e2005d0"
+    },
+    "dob": "1975-03-01 02:24:06",
+    "registered": "2005-01-01 06:03:52",
+    "phone": "017687 56806",
+    "cell": "0756-004-404",
+    "id": {
+      "name": "NINO",
+      "value": "SY 52 42 47 V"
+    },
+    "picture": {
+      "large": "https://randomuser.me/api/portraits/women/85.jpg",
+      "medium": "https://randomuser.me/api/portraits/med/women/85.jpg",
+      "thumbnail": "https://randomuser.me/api/portraits/thumb/women/85.jpg"
+    },
+    "nat": "GB"
+  },
+  {
+    "gender": "male",
+    "name": {
+      "title": "mr",
+      "first": "arcélio",
+      "last": "fogaça"
+    },
+    "location": {
+      "street": "8458 rua santos dumont ",
+      "city": "juiz de fora",
+      "state": "ceará",
+      "postcode": 78936
+    },
+    "email": "arcélio.fogaça@example.com",
+    "login": {
+      "username": "tinyduck758",
+      "password": "citadel",
+      "salt": "onIetglC",
+      "md5": "98b7367a12105e1fe8a816a688357772",
+      "sha1": "0e78d29a8c5137d1f562effcf887709447dbe2e6",
+      "sha256": "b202ab950d95cdae94dbf12f313f69d8338bfc8217545227d13cd9019fac09c6"
+    },
+    "dob": "1945-04-05 21:10:08",
+    "registered": "2013-05-17 05:22:18",
+    "phone": "(20) 8062-0434",
+    "cell": "(98) 9158-9071",
+    "id": {
+      "name": "",
+      "value": null
+    },
+    "picture": {
+      "large": "https://randomuser.me/api/portraits/men/58.jpg",
+      "medium": "https://randomuser.me/api/portraits/med/men/58.jpg",
+      "thumbnail": "https://randomuser.me/api/portraits/thumb/men/58.jpg"
+    },
+    "nat": "BR"
+  },
+  {
+    "gender": "female",
+    "name": {
+      "title": "mrs",
+      "first": "آدرینا",
+      "last": "گلشن"
+    },
+    "location": {
+      "street": "7133 کمیل",
+      "city": "خوی",
+      "state": "مازندران",
+      "postcode": 44449
+    },
+    "email": "آدرینا.گلشن@example.com",
+    "login": {
+      "username": "heavymouse832",
+      "password": "finish",
+      "salt": "xy6cBUV7",
+      "md5": "1206a2c934abfae1d13d4004c1276227",
+      "sha1": "0dafcf69f29c1be57e13141e50431bd1e5b9605f",
+      "sha256": "59296655602647a9064db0e3d144e84a312cecc016a69ee00a0da36c77ffc676"
+    },
+    "dob": "1962-12-17 08:51:50",
+    "registered": "2005-08-01 03:14:14",
+    "phone": "013-02196528",
+    "cell": "0939-080-2318",
+    "id": {
+      "name": "",
+      "value": null
+    },
+    "picture": {
+      "large": "https://randomuser.me/api/portraits/women/83.jpg",
+      "medium": "https://randomuser.me/api/portraits/med/women/83.jpg",
+      "thumbnail": "https://randomuser.me/api/portraits/thumb/women/83.jpg"
+    },
+    "nat": "IR"
+  },
+  {
+    "gender": "male",
+    "name": {
+      "title": "monsieur",
+      "first": "louis",
+      "last": "morin"
+    },
+    "location": {
+      "street": "1805 avenue jean-jaurès",
+      "city": "cully",
+      "state": "fribourg",
+      "postcode": 5673
+    },
+    "email": "louis.morin@example.com",
+    "login": {
+      "username": "greenbear149",
+      "password": "corndog",
+      "salt": "j1w28Kas",
+      "md5": "bb84fe8eed395c643a5009c55035306f",
+      "sha1": "6620e8859657df15b9a5f2c565af9363f4d1d34d",
+      "sha256": "07e5e14acd688aa53861afc46b55b7aded1be81d04d23f1eefb64b8732c63d7c"
+    },
+    "dob": "1978-11-13 01:26:10",
+    "registered": "2005-10-11 06:31:38",
+    "phone": "(349)-397-4312",
+    "cell": "(237)-570-0330",
+    "id": {
+      "name": "AVS",
+      "value": "756.TUIM.BWIH.20"
+    },
+    "picture": {
+      "large": "https://randomuser.me/api/portraits/men/54.jpg",
+      "medium": "https://randomuser.me/api/portraits/med/men/54.jpg",
+      "thumbnail": "https://randomuser.me/api/portraits/thumb/men/54.jpg"
+    },
+    "nat": "CH"
+  },
+  {
+    "gender": "male",
+    "name": {
+      "title": "mr",
+      "first": "محمدطاها",
+      "last": "سلطانی نژاد"
+    },
+    "location": {
+      "street": "8557 شهید صابونچی",
+      "city": "قم",
+      "state": "همدان",
+      "postcode": 67010
+    },
+    "email": "محمدطاها.سلطانینژاد@example.com",
+    "login": {
+      "username": "redladybug925",
+      "password": "niceguy",
+      "salt": "N9xuNfvV",
+      "md5": "63d28fbe0712aabbdb69dd9f4608f6b5",
+      "sha1": "71bb92e09928068193d0f621466e52e20da5c763",
+      "sha256": "db32a20638b8b58d7ae012d6138dbd7222f49fa2298e9d2ecdc22ad8d9f87c4c"
+    },
+    "dob": "1969-10-02 14:52:37",
+    "registered": "2004-11-13 21:20:14",
+    "phone": "022-23858560",
+    "cell": "0916-579-5699",
+    "id": {
+      "name": "",
+      "value": null
+    },
+    "picture": {
+      "large": "https://randomuser.me/api/portraits/men/36.jpg",
+      "medium": "https://randomuser.me/api/portraits/med/men/36.jpg",
+      "thumbnail": "https://randomuser.me/api/portraits/thumb/men/36.jpg"
+    },
+    "nat": "IR"
+  },
+  {
+    "gender": "female",
+    "name": {
+      "title": "miss",
+      "first": "sonia",
+      "last": "beck"
+    },
+    "location": {
+      "street": "4912 bruce st",
+      "city": "broken arrow",
+      "state": "utah",
+      "postcode": 10215
+    },
+    "email": "sonia.beck@example.com",
+    "login": {
+      "username": "bigbutterfly910",
+      "password": "sunset",
+      "salt": "PEpNGZNf",
+      "md5": "5b315ced1439b82cd32ecea791f213f5",
+      "sha1": "3cabdf841cc08cc20d6f581b8f5792dcc6060676",
+      "sha256": "877485866f3d9d1f06020d772192c49119bebe240e08e4b02cb9c506bad51596"
+    },
+    "dob": "1955-06-03 00:31:46",
+    "registered": "2015-05-28 13:31:55",
+    "phone": "(759)-726-2699",
+    "cell": "(895)-170-7034",
+    "id": {
+      "name": "SSN",
+      "value": "937-79-7379"
+    },
+    "picture": {
+      "large": "https://randomuser.me/api/portraits/women/60.jpg",
+      "medium": "https://randomuser.me/api/portraits/med/women/60.jpg",
+      "thumbnail": "https://randomuser.me/api/portraits/thumb/women/60.jpg"
+    },
+    "nat": "US"
+  },
+  {
+    "gender": "female",
+    "name": {
+      "title": "miss",
+      "first": "jamie",
+      "last": "mendoza"
+    },
+    "location": {
+      "street": "3843 e sandy lake rd",
+      "city": "helena",
+      "state": "new mexico",
+      "postcode": 69752
+    },
+    "email": "jamie.mendoza@example.com",
+    "login": {
+      "username": "smallbird495",
+      "password": "arianna",
+      "salt": "6IUVeT33",
+      "md5": "ffaf86cba6db153fae92a1740357b8fe",
+      "sha1": "dd93a239600cdf08c74a7fb0710013a0975de2d4",
+      "sha256": "e30bbb9eaa3db533b4cd96bc9bd8859fe7a849f7967180509b0b696718bf9fdd"
+    },
+    "dob": "1964-08-24 04:17:47",
+    "registered": "2015-01-24 17:45:14",
+    "phone": "(442)-693-8717",
+    "cell": "(857)-760-4137",
+    "id": {
+      "name": "SSN",
+      "value": "564-20-8990"
+    },
+    "picture": {
+      "large": "https://randomuser.me/api/portraits/women/95.jpg",
+      "medium": "https://randomuser.me/api/portraits/med/women/95.jpg",
+      "thumbnail": "https://randomuser.me/api/portraits/thumb/women/95.jpg"
+    },
+    "nat": "US"
+  }
+]
+
+export default UserData;
