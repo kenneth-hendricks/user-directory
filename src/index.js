7f7249a7c20d8868c67a46655866181a131ecab3
kenneth-hendricks <ken.c.hendricks@gmail.com>
2018-05-26 17:01:59 +1000

    Create store with UserData

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore } from 'redux';
+import UserData from './UserData';
 import App from './App';
+
+const store = createStore(
+  state => state,
+  UserData,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 render(
   <App />,
