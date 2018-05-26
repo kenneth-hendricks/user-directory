a5c966397e28c422d33a56becd1b2bf8fa00583e
kenneth-hendricks <ken.c.hendricks@gmail.com>
2018-05-26 17:09:23 +1000

    Render placeholder UserList

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
+import UserList from './components/UserList';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <UserList />
+  </div>
+);
 
 export default App;
---
+++ src/components/UserList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UserList = () => (
+  <div>User list</div>
+);
+
+export default UserList;
