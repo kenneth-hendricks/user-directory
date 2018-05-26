c3d7f7e3e203b3da3b10e71bca8c2859be92156f
kenneth-hendricks <ken.c.hendricks@gmail.com>
2018-05-26 17:30:51 +1000

    Render names of all users in UserList

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const UserList = () => (
-  <div>User list</div>
+const UserList = ({ userList }) => (
+  <ul>
+    {userList.map((user, i) => (
+      <li key={i}>{user.name.title} {user.name.first} {user.name.last}</li>
+    ))}
+  </ul>
 );
 
-export default UserList;
+const mapStateToProps = (state) => ({
+  userList: state
+})
+
+export default connect(mapStateToProps) (UserList);
