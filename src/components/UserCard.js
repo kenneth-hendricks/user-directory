485a68a5c173251fefa27365d2d1e2852bc8b09f
kenneth-hendricks <ken.c.hendricks@gmail.com>
2018-07-07 20:51:52 +1000

    Add email to user cards

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -4,7 +4,8 @@ import { capitalizeFirstLetter } from '../utils';
 const UserCard = ({ user }) => (
   <div className="card user">
     <div className="card-body">
-      {displayName(user)}
+      <h5 className="card-title">{displayName(user)}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">{user.email}</h6>
     </div>
   </div>
 );
--- src/components/UserGrid.js
+++ src/components/UserGrid.js
@@ -5,7 +5,7 @@ import UserCard from './UserCard';
 const UserGrid = ({ users }) => (
   <div className="row">
     {users.map((user, i) => (
-      <div className="col-lg-3 col-md-4 col-sm-6" key={i}>
+      <div className="col-lg-4 col-md-6" key={i}>
         <UserCard user={user} />
       </div>
     ))}
