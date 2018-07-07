f1b726e4a89115c9b5b88c311417ae96e6713c26
kenneth-hendricks <ken.c.hendricks@gmail.com>
2018-07-07 14:36:47 +1000

    Set user card responsive width classes

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -5,7 +5,7 @@ import UserCard from './UserCard';
 const UserList = ({ userList }) => (
   <div className="row">
     {userList.map((user, i) => (
-      <div className="col-3" key={i}>
+      <div className="col-lg-3 col-md-4 col-sm-6" key={i}>
         <UserCard user={user} />
       </div>
     ))}
