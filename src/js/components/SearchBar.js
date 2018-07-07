5e0ca04c59581739707eaec680fd1dc1441a4126
kenneth-hendricks <ken.c.hendricks@gmail.com>
2018-07-08 08:23:16 +1000

    Fix indentation

--- src/js/components/SearchBar.js
+++ src/js/components/SearchBar.js
@@ -5,19 +5,19 @@ const SearchBar = ({ updateSearchTerm }) => {
   let input;
   return (
     <nav className="navbar navbar-light bg-light">
-    <div className="container">
-      <a className="navbar-brand" href="#">UserDirectory</a>
-      <form className="form-inline">
-        <input
-          className="form-control"
-          type="search"
-          placeholder="Search"
-          aria-label="Search"
-          ref={node => input = node}
-          onChange={() => updateSearchTerm(input.value)}/>
-      </form>
-    </div>
-  </nav>
+      <div className="container">
+        <a className="navbar-brand" href="#">UserDirectory</a>
+        <form className="form-inline">
+          <input
+            className="form-control"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+            ref={node => input = node}
+            onChange={() => updateSearchTerm(input.value)}/>
+        </form>
+      </div>
+    </nav>
   );
 };
 
