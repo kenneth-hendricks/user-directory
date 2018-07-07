1ca137bf1a19ff097826cee5b31e24ef9511fa3d
kenneth-hendricks <ken.c.hendricks@gmail.com>
2018-07-07 17:38:44 +1000

    Add searchTerm reducer

---
+++ src/reducers/searchTerm.js
@@ -0,0 +1,10 @@
+const searchTerm = (state = '', action) => {
+  switch (action.type) {
+    case 'UPDATE_SEARCH_TERM':
+      return action.searchTerm;
+    default:
+      return state;
+  }
+}
+
+export default searchTerm;
